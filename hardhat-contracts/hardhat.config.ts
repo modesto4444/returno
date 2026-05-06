2341b78ac930b67a3843d073d2fe48ebfcf720cb
lethaljo <133568712+lethaljo@users.noreply.github.com>
2026-05-06 09:51:19 +0800

    Add files via upload

---
+++ hardhat-contracts/hardhat.config.ts
@@ -0,0 +1,8 @@
+import "@nomicfoundation/hardhat-ethers";
+import type { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+};
+
+export default config;
